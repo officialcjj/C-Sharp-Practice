9727da5431d57b105acc17f242af633dbcfd3ca5
officialcjj <116774757+officialcjj@users.noreply.github.com>
2023-03-09 17:29:07 -0800

    Initial commit

---
+++ MethodsdPractice/MethodsdPractice/Object.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsdPractice
+{
+    // creates an Object class
+    public class Object
+    {
+        public int number; // adds integer field to Object
+        public string name; // adds string field to Object
+        public string quality;
+
+        // constructor for Object
+        public Object(string objectName, int objectNumber, string objectQuality)
+        {
+            name = objectName;
+            number = objectNumber;
+            quality = objectQuality;
+        }
+
+        public static int Addition(Object userObject, int added)
+        {
+            added = userObject.number + 10;
+            Console.WriteLine("Your object's number plus ten equals " + added); 
+            return added;
+        }
+
+        public static int Multiplication(Object userObject, int multiplied)
+        {
+            multiplied = userObject.number * 10;
+            Console.WriteLine("Your object's number times ten equals " + multiplied);
+            return multiplied;
+        }
+
+        public static int Division(Object userObject, int divided)
+        {
+            divided = userObject.number / 4;
+            Console.WriteLine("Your object's number divided by four equals " + divided);
+            return divided;
+        }
+
+        // allows user to update name of userOject
+        public static string NameChange(Object userObject)
+        {
+            Console.WriteLine("Change the name of your object? (Enter 'Yes' or 'No')");
+            string answer = Console.ReadLine();
+            if (answer == "Yes") // prompts user to enter new name and update userObject.name
+            {
+                Console.WriteLine("Enter the new name for your object:");
+                userObject.name = Console.ReadLine();
+                Console.WriteLine("Your object's new name is: " + userObject.name);
+                return userObject.name;
+            }
+            else // returns unupdated name
+            {
+                Console.WriteLine("Your object's name is still " + userObject.name);
+                return userObject.name;
+            }
+        }
+    }
+}
---
+++ MethodsdPractice/MethodsdPractice/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsdPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // gets user inputs
+            Console.WriteLine("Create an object with a name, number, and quality!\nEnter a name:");
+            string objectName = Console.ReadLine();
+            Console.WriteLine("Enter an integer:");
+            int objectNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a quality:");
+            string objectQuality = Console.ReadLine();
+
+            // creates and describes user object
+            Object userObject = new Object(objectName, objectNumber, objectQuality);
+            Console.WriteLine("You created a " + userObject.name + " with the number " + userObject.number + " and the quality " + userObject.quality);
+
+            int added = 0; //initializes variable because static methods don't automatically
+            added = Object.Addition(userObject, added); //sends user input and preinitialized variable to method
+            Console.WriteLine(added);
+
+            int multiplied = 0;
+            multiplied = Object.Multiplication(userObject, multiplied);
+            Console.WriteLine(multiplied);
+
+            int divided = 0;
+            divided = Object.Division(userObject, divided);
+            Console.WriteLine(divided);
+
+            // calls NameChange to update userObject.name
+            Object.NameChange(userObject);
+
+            Console.ReadLine();
+        }
+    }
+}
