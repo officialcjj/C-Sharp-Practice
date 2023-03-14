0479ee377bce55491f9c5ec80cb14c07c0d2ada2
officialcjj <116774757+officialcjj@users.noreply.github.com>
2023-03-13 19:02:14 -0800

    Initial commit

---
+++ Clinheritance/Clinheritance/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Clinheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("Sample", "Student", 0);
+            Student student = new Student("Homie", "Jones", 0);
+            employee.SayName();
+            student.SayName();
+            Console.ReadLine();
+        }
+    }
+}
