4be31065708407e445de7a9392835a3ef3c5e8bd
officialcjj <116774757+officialcjj@users.noreply.github.com>
2023-03-16 15:56:07 -0800

    Initial commit

---
+++ ConstructorChain/ConstructorChain/ObjectClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChain
+{
+    public class ObjectClass
+    {
+        public string Name;
+        public int ID;
+        public ObjectClass()
+        {
+            //no parameters
+        }
+        public ObjectClass(string name)
+        {
+            Name = name;
+        }
+        public ObjectClass(string name, int id)
+        {
+            Name = name;
+            ID = id;
+        }
+    }
+}
